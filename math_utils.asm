d542aa3d5ba49672676f7c28bb9f24bcc2b3ba41
hosny8 <157162887+hosny8@users.noreply.github.com>
2024-11-22 17:52:32 -0800

    ..

---
+++ math_utils.asm
@@ -0,0 +1,12 @@
+global relu, dot, matrix_multiplication
+
+section .text
+
+relu:
+    ; ReLU implementation (already translated)
+
+dot:
+    ; Dot product implementation (already translated)
+
+matrix_multiplication:
+    ; Matrix multiplication implementation (already translated)
